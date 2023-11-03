690aa94c6d16c27cef10554dbdee1a1c4b4ad01d
Sadaf-Tariq <>
2023-11-03 11:59:42 +0000

    Add custom user model to users app and create superuser

--- desirecipeblog/settings.py
+++ desirecipeblog/settings.py
@@ -34,8 +34,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-sadaf-tariq-pp4-desi-rec-mf9nilybq5.us2.codeanyapp.com', 'pp4-desi-recipe-blog-04aed9a2aa93.herokuapp.com', 'localhost']
+    '8000-sadaf-tariq-pp4-desi-rec-mf9nilybq5.us2.codeanyapp.com', 'https://pp4-desi-recipe-blog-04aed9a2aa93.herokuapp.com', 'localhost']
 
+AUTH_USER_MODEL = "users.CustomUser"
 
 # Application definition
 
