0bc81dd351445f61cd3a858cf0f29f311ae6213a
Sadaf-Tariq <>
2023-11-05 23:47:32 +0000

    Add comment and view comment view and templates

--- recipeblog/views.py
+++ recipeblog/views.py
@@ -40,7 +40,7 @@ def full_recipe(request, slug, *args, **kwargs):
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
-            comment_form.instance.name = request.user.username
+            # comment_form.instance.name = request.user.username
             comment = comment_form.save(commit=False)
             comment.recipe = recipe
             comment.save()
