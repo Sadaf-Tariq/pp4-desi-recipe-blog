3d3c2f6a593aa5581854d8d76d7388e5d67257b0
Sadaf-Tariq <>
2023-11-07 21:50:07 +0000

    Add create new recipe temple, form view

--- recipeblog/forms.py
+++ recipeblog/forms.py
@@ -1,6 +1,6 @@
 from .models import Comment, Recipe
 from django import forms
-
+from django_summernote.widgets import SummernoteWidget
 
 class CommentForm(forms.ModelForm):
     class Meta:
@@ -11,5 +11,14 @@ class CommentForm(forms.ModelForm):
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ('title', 'category','method', 'author_name', 'featured_image', 'ingredients', 'instructions', 
-        'prep_time', 'cooking_time', 'servings', 'calories')
+        fields = ('title', 'category', 'method', 'author_name', 
+                  'featured_image', 'ingredients', 'instructions',
+                  'prep_time', 'cooking_time', 'servings', 'calories')
+        widgets = {
+            'instructions': SummernoteWidget(),
+            'ingredients': SummernoteWidget(),
+            'prep_time': forms.NumberInput(
+                attrs={'placeholder': 'Input in minutes'}),
+            'cooking_time': forms.NumberInput(
+                attrs={'placeholder': 'Input in minutes'})
+        }
