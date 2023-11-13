d263da08ae5adda1206e0b8905f7e5c0dc658076
Sadaf Tariq <sadaf.t.tarique@gmail.com>
2023-11-13 01:28:43 +0000

    update settings

--- desirecipeblog/urls.py
+++ desirecipeblog/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 admin.autodiscover()
@@ -24,4 +26,4 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('', include('recipeblog.urls'), name='recipeblog_urls'),
     path('accounts/', include('allauth.urls')),
-]
+] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
