9205ee233b005beca7b05ded37b09de2aa0fdf29
Sadaf-Tariq <>
2023-11-13 22:59:37 +0000

    Fix console error

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,25 +1,17 @@
-
-setTimeout(function () {
-    let messages = document.getElementById('msg');
-    let alert = new bootstrap.Alert(messages);
-    alert.close();
-}, 3000)
-
-
 function leftScroll() {
-  const left = document.querySelector(".scroll-imagesCat");
-  left.scrollBy(200, 0);
+    const left = document.querySelector(".scroll-imagesCat");
+    left.scrollBy(200, 0);
 }
 function rightScroll() {
-  const right = document.querySelector(".scroll-imagesCat");
-  right.scrollBy(-200, 0);
+    const right = document.querySelector(".scroll-imagesCat");
+    right.scrollBy(-200, 0);
 }
 
 function leftScrollM() {
-  const left = document.querySelector(".scroll-imagesMethod");
-  left.scrollBy(200, 0);
+    const left = document.querySelector(".scroll-imagesMethod");
+    left.scrollBy(200, 0);
 }
 function rightScrollM() {
-  const right = document.querySelector(".scroll-imagesMethod");
-  right.scrollBy(-200, 0);
+    const right = document.querySelector(".scroll-imagesMethod");
+    right.scrollBy(-200, 0);
 }
